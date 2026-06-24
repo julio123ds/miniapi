de663dbab135b3ea8c532fd68ffaee67e3b3db10
julio123ds <80724081+julio123ds@users.noreply.github.com>
2026-06-23 22:42:06 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+
+    if(window.scrollY > 50){
+        header.style.background = '#000';
+    } else {
+        header.style.background = 'rgba(0,0,0,.9)';
+    }
+});
